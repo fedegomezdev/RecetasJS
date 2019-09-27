2f39ff728adcc5911ebeeba81ba6135ac5ee9a7a
Fede <fedegomezdev@gmail.com>
2019-09-27 12:44:42 -0300

    cambiando la rta de /

--- src/routes/recetasR.js
+++ src/routes/recetasR.js
@@ -2,7 +2,7 @@ const {Router} = require('express');
 const router = Router(); //para iniciar el enrutador de express
 
 router.get('/', (req,res) =>{
-    res.send('holas');
+    res.send('Probando algo');
 } )
 
 
