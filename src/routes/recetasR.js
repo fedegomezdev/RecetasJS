37b6ef4ff0073df226952d454677bf9a9c3edbd4
fede <50002020+fedegomezdev@users.noreply.github.com>
2019-10-04 12:57:51 -0300

    agrregando 2 endpoints al controller

--- src/routes/recetasR.js
+++ src/routes/recetasR.js
@@ -1,9 +1,20 @@
 const {Router} = require('express');
 const router = Router(); //para iniciar el enrutador de express
 
-router.get('/', (req,res) =>{
-    res.send('Probando algo de git ');
-} )
+recetascrl.getRecetas = async (req, res) => {
+    const recetas = await Recetas.find()
+    res.json(recetas);
+}
+
+recetascrl.createReceta = async(req, res) => {
+    const { titulo, contenido } = req.body;
+    const newReceta = await new Recetas({
+        titulo : titulo,
+        contenido: contenido
+    })
+    await newReceta.save();
+    res.send('ready')
+}
 
 
 module.exports = router;
